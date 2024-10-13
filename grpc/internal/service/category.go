0f507316b9aee02a4e220ec0850619180d11731d
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2024-10-13 16:38:30 -0300

    added some unimplemented methods

--- grpc/internal/service/category.go
+++ grpc/internal/service/category.go
@@ -30,3 +30,36 @@ func (c *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCateg
 		Description: category.Description,
 	}, nil
 }
+
+func (c *CategoryService) ListCategories(ctx context.Context, in *pb.Blank) (*pb.CategoryList, error) {
+	categories, err := c.CategoryDB.FindAll()
+	if err != nil {
+		return nil, err
+	}
+
+	var pbCategories []*pb.Category
+	for _, category := range categories {
+		pbCategories = append(pbCategories, &pb.Category{
+			Id:          category.ID,
+			Name:        category.Name,
+			Description: category.Description,
+		})
+	}
+
+	return &pb.CategoryList{
+		Categories: pbCategories,
+	}, nil
+}
+
+func (c *CategoryService) GetCategory(ctx context.Context, in *pb.GetCategoryRequest) (*pb.Category, error) {
+	category, err := c.CategoryDB.Find(in.Id)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.Category{
+		Id:          category.ID,
+		Name:        category.Name,
+		Description: category.Description,
+	}, nil
+}
